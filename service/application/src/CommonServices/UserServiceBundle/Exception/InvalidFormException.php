76798a47ca58e86e85e5a71e17a0262ef7fa7789
Mohamed R. Almasry <almasry@almasry.ws>
2017-03-05 14:16:49 +0100

    Fixed errors json response handler

--- service/application/src/CommonServices/UserServiceBundle/Exception/InvalidFormException.php
+++ service/application/src/CommonServices/UserServiceBundle/Exception/InvalidFormException.php
@@ -2,8 +2,12 @@
 
 namespace CommonServices\UserServiceBundle\Exception;
 
+use Geocoder\Exception\HttpError;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormErrorIterator;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 /**
@@ -27,16 +31,48 @@ class InvalidFormException extends \Exception
     {
         $errors =[];
 
-        foreach ($formErrors as $form){
-            $errors[$formErrors->getForm()->getName()]=
-                [
-                    $formErrors->getChildren()
-                ];
+        $form = $formErrors->getForm();
+
+        foreach ($form->getErrors() as $error) {
+            $errors['global'] = (string) $error->getMessage();
+        }
+        // Fields
+        foreach ($form as $child /** @var Form $child */) {
+            foreach ($child as $element) {
+                if (!$element->isValid()) {
+                    foreach ($element->getErrors() as $error) {
+                        $errors[$child->getName()]['message'] = $error->getMessage();
+                        $errors[$child->getName()]['messagePluralization'] = $error->getMessagePluralization();
+                        //$errors[$child->getName()]['messageParameters'] = $error->getMessageParameters();
+                        $errors[$child->getName()]['cause'] = $errors[$child->getName()];
+                        $errorCause = $error->getCause();
+                        $errors[$child->getName()]['cause']['plural'] = $errorCause->getPlural();
+                        $errors[$child->getName()]['cause']['propertyPath'] = $errorCause->getPropertyPath();
+                        $errors[$child->getName()]['cause']['invalidValue'] = $errorCause->getInvalidValue();
+                        $errors[$child->getName()]['cause']['code'] = $errorCause->getCode();
+                        $errors[$child->getName()]['cause']['cause'] = $errorCause->getCause();
+                    }
+                }
+            }
+            if (!$child->isValid()) {
+                foreach ($child->getErrors() as $error) {
+                    $errors[$child->getName()]['message'] = $error->getMessage();
+                    $errors[$child->getName()]['messagePluralization'] = $error->getMessagePluralization();
+                    //$errors[$child->getName()]['messageParameters'] = $error->getMessageParameters();
+                    $errors[$child->getName()]['cause'] = $errors[$child->getName()];
+                    if ($errorCause = $error->getCause()) {
+                        $errors[$child->getName()]['cause']['plural'] = $errorCause->getPlural();
+                        $errors[$child->getName()]['cause']['propertyPath'] = $errorCause->getPropertyPath();
+                        $errors[$child->getName()]['cause']['invalidValue'] = $errorCause->getInvalidValue();
+                        $errors[$child->getName()]['cause']['code'] = $errorCause->getCode();
+                        $errors[$child->getName()]['cause']['cause'] = $errorCause->getCause();
+                    }
+                }
+            }
         }
 
-        self::$formErrors = $errors;
-
-        parent::__construct($message);
+        self::$formErrors =  $errors;
+        parent::__construct($message, Response::HTTP_BAD_REQUEST);
     }
 
     /**
