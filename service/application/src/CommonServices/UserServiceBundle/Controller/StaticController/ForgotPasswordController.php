f383f2d0feb6c85d1577326a0254819f26c0d1cb
engmg2011 <eng.mg2011@gmail.com>
2017-03-23 09:14:28 +0200

    latest my works before 23 march

---
+++ service/application/src/CommonServices/UserServiceBundle/Controller/StaticController/ForgotPasswordController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CommonServices\UserServiceBundle\Controller\StaticController;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ForgotPasswordController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @ApiDoc(
+     *  section="Static Resources",
+     *  description="Static forgotpassword page that renders a static HTML page",
+     *  output="Symfony\Component\HttpFoundation\Response",
+     *  tags={"beta"},
+     *  statusCodes={
+     *         200="Page was rendered successfully",
+     *         500="The system is unable to process the request due to a server side error"
+     *  }
+     * )
+     */
+    public function forgotPasswordAction()
+    {
+        return $this->render('UserServiceBundle::forgot-password.html.twig');
+    }
+}
