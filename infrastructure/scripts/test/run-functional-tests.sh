c43d310d2e6c3bf9dd0e1e01d61f8608ab8092df
Mohamed R. Almasry <almasry@almasry.ws>
2017-03-28 17:06:33 +0200

    fixed BEHAT_PARAMS bash script

--- infrastructure/scripts/test/run-functional-tests.sh
+++ infrastructure/scripts/test/run-functional-tests.sh
@@ -2,8 +2,6 @@
 
 ## setting mink base_url to run the functional tests
 
-docker exec -i user-service-php-fpm /bin/sh -c export BEHAT_PARAMS='{"extensions":{"Behat\\MinkExtension":{"base_url":"http://user-service.dev/app_test.php/"}}}'
-
 docker exec -i user-service-php-fpm /bin/sh -c "echo $BEHAT_PARAMS"
 docker exec -i user-service-php-fpm /bin/sh -c "echo ZZZZZZ"
 docker exec -i user-service-php-fpm /bin/sh -c ./vendor/bin/behat --profile default
