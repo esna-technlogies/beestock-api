7aecccfd836a190cb9a65b011e16d4c6948eb909
Mohamed R. Almasry <almasry@almasry.ws>
2017-03-28 17:36:19 +0200

    added fixed P assress for testing php docker

--- infrastructure/scripts/test/run-functional-tests.sh
+++ infrastructure/scripts/test/run-functional-tests.sh
@@ -2,7 +2,5 @@
 
 ## setting mink base_url to run the functional tests
 
-docker exec -i user-service-php-fpm /bin/sh -c "echo $BEHAT_PARAMS"
-docker exec -i user-service-php-fpm /bin/sh -c "echo ZZZZZZ"
 docker exec -i user-service-php-fpm /bin/sh -c ./vendor/bin/behat --profile default
 
