2c71c1f4ea5b81acaeac204952ae262525e00167
Mohamed R. Almasry <almasry@almasry.ws>
2017-10-11 02:04:15 +0200

    published events

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ docker-compose up --build -d
 
 cd ../../../
 
-rm -R ./service/application/var/cache/*
+sudo rm -R ./service/application/var/cache/*
 
-chmod 777 -R ./service/application/var
+sudo chmod 777 -R ./service/application/var
