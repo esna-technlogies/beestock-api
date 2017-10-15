e972d84aab8d364118bf259741801163220e37d6
Mohamed R. Almasry <almasry@almasry.ws>
2017-10-15 07:31:20 +0200

    added user redirection after activation

--- service/application/src/CommonServices/UserServiceBundle/Controller/ChangeRequest/ChangeRequestController.php
+++ service/application/src/CommonServices/UserServiceBundle/Controller/ChangeRequest/ChangeRequestController.php
@@ -60,15 +60,15 @@ class ChangeRequestController extends Controller
         $changeRequestService = $this->get('user_service.change_request_domain');
 
         if (is_null($changeRequest)) {
-            throw new NotFoundException("Change request not found", Response::HTTP_NOT_FOUND);
+            return $this->redirect($this->getParameter('cors_allow_origin'),'/?redirect_message=ACCOUNT_ACTIVATION_FAILED');
         }
 
         $changeRequestService->getChangeRequest($changeRequest)->executeChange($request->get('code', ''));
 
-        return new Response('', Response::HTTP_NO_CONTENT);
+        return $this->redirect($this->getParameter('cors_allow_origin'),'/?redirect_message=ACCOUNT_SUCCESSFULLY_ACTIVATED');
     }
 
-    /**
+/**
      * Verify code request handles account changes issued by user (code sent by SMS and via email)
      * @param ChangeRequest $changeRequest
      * @param Request $request
